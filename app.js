5c3bd96a9a108ca53ffb724482e966fce00a77a9
JYang <jyang15@mail.sfsu.edu>
2018-04-03 20:26:28 -0700

    implement login and register routes

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var test = require('./routes/test');
+var login = require('./routes/login');
+var register = require('./routes/register');
 
 var app = express();
 
@@ -30,6 +32,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/test', test);
+app.use('/login', login);
+app.use('/register', register);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
