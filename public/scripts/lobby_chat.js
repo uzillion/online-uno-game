a8c92258c47d1f57278abd6baa36d312a30b83cc
Jiawen Zhu <jzhu10@mail.sfsu.edu>
2018-04-16 21:53:00 -0700

    add a nick name for user to chat or add guest user

--- public/scripts/lobby_chat.js
+++ public/scripts/lobby_chat.js
@@ -1,18 +1,24 @@
 
 $(function () {
   var message = document.getElementById('m'),
-   userName = document.getElementById('nameInput');
+   userName = document.getElementById('nameInput')
+   
+   numUsers = 0;
     var socket = io('http://localhost:3000');
     $('form').submit(function(){
       console.log('message sent');
       socket.emit('chat message', {
       message: message.value,
-      name: "guest"
+      name: userName.value
     });
       // $('#m').val('')};
       return false;
     });
     socket.on('chat message', function(msg){
-      $('#messages').append('<strong><font color="#397AF2">' + msg.name + '</font></strong>: '+msg.message);
+      if (msg.name == ""){
+        msg.name = "guest user";
+      }
+      $('#messages').append('<li><strong><font color="#397AF2">' + msg.name + '</font></strong>: '+msg.message+'</li>');
+      document.getElementById('m').value = "";
     });
   });
